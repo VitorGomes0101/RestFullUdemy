1f86354d91179672a54f5502e4f18c10b86c491a
vitor_gomes_18@live.com <vitor_gomes_18@live.com>
2022-03-13 13:55:03 -0300

    [UPDATE] Adjustments in calculator

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -20,35 +20,53 @@ namespace RestWithASPNETUdemy.Controllers
             _logger = logger;
         }
 
-        [HttpGet("calc/{firstNumber}/{secondNumber}/{operation}")]
-        public IActionResult Get(string firstNumber, string secondNumber, string operation)
+        [HttpGet("sun/{firstNumber}/{secondNumber}")]
+        public IActionResult Sun(string firstNumber, string secondNumber)
         {
             if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber)) return BadRequest("Invalid Input");
-            
-            decimal calc = 0;
-            
-            switch (operation)
-            {
-                case "+":
-                    calc = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                    break;
-                case "-":
-                    calc = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
-                    break;
-                case "*":
-                    calc = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
-                    break;
-                case "%":
-                    if (ConvertToDecimal(firstNumber) == 0 || ConvertToDecimal(secondNumber) == 0)
-                        return BadRequest("Impossible to divide by zero");
-                    calc = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
-                    break;
-                case "m":
-                    calc = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber) / 2;
-                    break;
-            }
-            
-            return Ok(calc.ToString(CultureInfo.InvariantCulture));
+            var sun = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(sun.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber)) return BadRequest("Invalid Input");
+            var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+            return Ok(sub.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        [HttpGet("abs/{firstNumber}/{secondNumber}")]
+        public IActionResult Abs(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber)) return BadRequest("Invalid Input");
+            var abs = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+            return Ok(abs.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber)) return BadRequest("Invalid Input");
+            if (ConvertToDecimal(firstNumber) == 0 || ConvertToDecimal(secondNumber) == 0) return BadRequest("Division by 0");
+            var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+            return Ok(div.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        [HttpGet("avg/{firstNumber}/{secondNumber}")]
+        public IActionResult Avg(string firstNumber, string secondNumber)
+        {
+            if (!IsNumeric(firstNumber) || !IsNumeric(secondNumber)) return BadRequest("Invalid Input");
+            var avg = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+            return Ok(avg.ToString(CultureInfo.InvariantCulture));
+        }
+        
+        [HttpGet("sqrt/{number}")]
+        public IActionResult Sqrt(string number)
+        {
+            if (!IsNumeric(number)) return BadRequest("Invalid Input");
+            var sqrt = Math.Sqrt(ConvertToDouble(number));
+            return Ok(sqrt.ToString(CultureInfo.InvariantCulture));
         }
 
         private bool IsNumeric(string strNumber)
@@ -64,6 +82,10 @@ namespace RestWithASPNETUdemy.Controllers
         private decimal ConvertToDecimal(string strNumber)
         {
             return decimal.TryParse(strNumber, out var decimalValue) ? decimalValue : 0;
+        }
+        private double ConvertToDouble(string strNumber)
+        {
+            return double.TryParse(strNumber, out var decimalValue) ? decimalValue : 0;
         }
     }
 }
